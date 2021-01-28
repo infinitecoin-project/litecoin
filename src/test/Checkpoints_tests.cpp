422b72344592766cd7f42b158ea0e021b7ef61e7
infinitecoin-project <63966367+infinitecoin-project@users.noreply.github.com>
2021-01-29 01:03:31 +0800

    Add files via upload

--- src/test/Checkpoints_tests.cpp
+++ src/test/Checkpoints_tests.cpp
@@ -14,8 +14,8 @@ BOOST_AUTO_TEST_SUITE(Checkpoints_tests)
 
 BOOST_AUTO_TEST_CASE(sanity)
 {
-    uint256 p1500 = uint256("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967");
-    uint256 p120000 = uint256("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131");
+    uint256 p1500 = uint256("0x605a02ce718ced87b68a3e601081358c64091dc6e546f0e444cafe6e8f73cef5");
+    uint256 p120000 = uint256("0x00a7bdd2d47ba640fe8d1ef18638a8d7c77f179a0ed23e51fc83a71da2005b45");
     BOOST_CHECK(Checkpoints::CheckBlock(1500, p1500));
     BOOST_CHECK(Checkpoints::CheckBlock(120000, p120000));
 
