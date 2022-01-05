d4b6bcfc966f6f1d591b712251ec199f0935e40e
infinitecoin <63966367+infinitecoin-project@users.noreply.github.com>
2022-01-05 12:17:24 +0800

    Add files via upload

--- src/pow.cpp
+++ src/pow.cpp
@@ -1,40 +1,41 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
+// Copyright (c) 2009-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "pow.h"
 
+#include "arith_uint256.h"
 #include "chain.h"
-#include "chainparams.h"
 #include "primitives/block.h"
 #include "uint256.h"
 #include "util.h"
 
-unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock)
+unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
-    unsigned int nProofOfWorkLimit = Params().ProofOfWorkLimit().GetCompact();
+    unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
 
     // Genesis block
     if (pindexLast == NULL)
         return nProofOfWorkLimit;
+	
 if((pindexLast->nHeight+1) < 245000)
 {
-    // Only change once per interval
-    if ((pindexLast->nHeight+1) % Params().Interval() != 0)
+    // Only change once per difficulty adjustment interval
+    if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval() != 0)
     {
-        if (Params().AllowMinDifficultyBlocks())
+        if (params.fPowAllowMinDifficultyBlocks)
         {
             // Special difficulty rule for testnet:
             // If the new block's timestamp is more than 2* 10 minutes
             // then allow mining of a min-difficulty block.
-            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + Params().TargetSpacing()*2)
+            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
                 return nProofOfWorkLimit;
             else
             {
                 // Return the last non-special-min-difficulty-rules-block
                 const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % Params().Interval() != 0 && pindex->nBits == nProofOfWorkLimit)
+                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
                     pindex = pindex->pprev;
                 return pindex->nBits;
             }
@@ -43,119 +44,130 @@ if((pindexLast->nHeight+1) < 245000)
     }
 }
 
-    int64_t nIntervalPPC = 30;
+   
     int64_t nActualTimespan = 30 * 120;
+    const CBlockIndex* pindexFirst = pindexLast;
+	
+    if((pindexLast->nHeight+1) < 272000)
+    {   
+	// Go back the full period unless it's the first retarget after genesis. Code courtesy of Art Forz
+        int blockstogoback = params.DifficultyAdjustmentInterval()-1;
+        if ((pindexLast->nHeight+1) != params.DifficultyAdjustmentInterval())
+            blockstogoback = params.DifficultyAdjustmentInterval();
+	    
+	// Go back by what we want to be blockstogoback worth of blocks
 	const CBlockIndex* pindexFirst = pindexLast;
-
-	if((pindexLast->nHeight+1) < 272000)
-	{
-        // Litecoin: This fixes an issue where a 51% attack can change difficulty at will.
-        // Go back the full period unless it's the first retarget after genesis. Code courtesy of Art Forz
-        int blockstogoback = Params().Interval()-1;
-        if ((pindexLast->nHeight+1) != Params().Interval())
-            blockstogoback = Params().Interval();
-
-        // Go back by what we want to be 14 days worth of blocks
-        const CBlockIndex* pindexFirst = pindexLast;
-        for (int i = 0; pindexFirst && i < blockstogoback; i++)
+	for (int i = 0; pindexFirst && i < blockstogoback; i++)
             pindexFirst = pindexFirst->pprev;
         assert(pindexFirst);
-
+	    
         // Limit adjustment step
-        nActualTimespan = pindexLast->GetBlockTime() - pindexFirst->GetBlockTime();
+        nActualTimespan = pindexLast->GetBlockTime() - pindexFirst-> GetBlockTime ();
         //LogPrintf("  nActualTimespan = %d  before bounds\n", nActualTimespan);
-
-	    if((pindexLast->nHeight+1) < 1500)
-	    {
-            if (nActualTimespan < Params().TargetTimespan()/16)
-                nActualTimespan = Params().TargetTimespan()/16;
+        if((pindexLast->nHeight+1) < 1500)
+        {
+            if (nActualTimespan < params.nPowTargetTimespan/16)
+                nActualTimespan = params.nPowTargetTimespan/16;
         }
-		else
-		{
-            if (nActualTimespan < Params().TargetTimespan()/4)
-                nActualTimespan = Params().TargetTimespan()/4;
-		} 
-		if (nActualTimespan > Params().TargetTimespan()*4)
-            nActualTimespan = Params().TargetTimespan()*4;
-	}
-	else	// PPCoin formula with 1 block time
-	{
-		// get the previous block
-		pindexFirst = pindexLast->pprev;
-		nActualTimespan = (pindexLast->GetBlockTime() - pindexFirst->GetBlockTime()) * Params().Interval();
-
-		// limit the adjustment
-		if (nActualTimespan < Params().TargetTimespan()/16)
-            nActualTimespan = Params().TargetTimespan()/16;
-        if (nActualTimespan > Params().TargetTimespan()*16)
-            nActualTimespan = Params().TargetTimespan()*16;
+	else
+        {
+            if (nActualTimespan < params.nPowTargetTimespan/4)
+                nActualTimespan = params.nPowTargetTimespan/4;
+	} 
+	if (nActualTimespan > params.nPowTargetTimespan*4)
+            nActualTimespan = params.nPowTargetTimespan*4;
     }
+    else	// PPCoin formula with 1 block time
+    {
+	// get the previous block
+	pindexFirst = pindexLast->pprev;
+	nActualTimespan = (pindexLast->GetBlockTime() - pindexFirst-> GetBlockTime ()) * params.DifficultyAdjustmentInterval();
+
+	// limit the adjustment
+        if (nActualTimespan < params.nPowTargetTimespan/16)
+            nActualTimespan = params.nPowTargetTimespan/16;
+        if (nActualTimespan > params.nPowTargetTimespan*16)
+            nActualTimespan = params.nPowTargetTimespan*16;
+    }
+	    
 
     // Retarget
-    uint256 bnNew;
-	uint256 bnOld;
+    arith_uint256 bnNew;
+    arith_uint256 bnOld;
     bnNew.SetCompact(pindexLast->nBits);
-	bnOld = bnNew;
+    bnOld = bnNew;
 	
-    if((pindexLast->nHeight+1) < 248000)                          // 120-block linear retarget
-	{
+    if((pindexLast->nHeight+1) < 248000)                      // 120-block linear retarget
+    {
         bnNew *= nActualTimespan;
-        bnNew /= Params().TargetTimespan();
-	}
-	else                                                          // PPCoin retarget algorithm
-	{
-		bnNew *= ((nIntervalPPC - 1) * Params().TargetTimespan() + nActualTimespan + nActualTimespan);
-		bnNew /= ((nIntervalPPC + 1) * Params().TargetTimespan());
-	}
-
-    if (bnNew > Params().ProofOfWorkLimit())
-        bnNew = Params().ProofOfWorkLimit();
+        bnNew /= params.nPowTargetTimespan;
+    }
+    else                                                      // PPCoin retarget algorithm
+    {
+	bnNew *= ((30 - 1) * params.nPowTargetTimespan + nActualTimespan + nActualTimespan);
+	bnNew /= ((30 + 1) * params.nPowTargetTimespan);
+    }
+	
+    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
+    if (bnNew > bnPowLimit)
+        bnNew = bnPowLimit;
 
     /// debug print
     LogPrintf("GetNextWorkRequired RETARGET\n");
-    LogPrintf("Params().TargetTimespan() = %d    nActualTimespan = %d\n", Params().TargetTimespan(), nActualTimespan);
+    LogPrintf("params.nPowTargetTimespan = %d    nActualTimespan = %d\n", params.nPowTargetTimespan, nActualTimespan);
     LogPrintf("Before: %08x  %s\n", pindexLast->nBits, bnOld.ToString());
     LogPrintf("After:  %08x  %s\n", bnNew.GetCompact(), bnNew.ToString());
 
-    return bnNew.GetCompact();
+    return bnNew.GetCompact();	
 }
 
-bool CheckProofOfWork(uint256 hash, unsigned int nBits)
+bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
 {
-    if(hash.GetHex() == "55ca007399d28cd3e3d921709ee6c52665db5cbf4cf0865230a00d2992c6812b")
-            return true;
-
     bool fNegative;
     bool fOverflow;
-    uint256 bnTarget;
-
-    if (Params().SkipProofOfWorkCheck())
-       return true;
+    arith_uint256 bnTarget;
 
     bnTarget.SetCompact(nBits, &fNegative, &fOverflow);
 
     // Check range
-    if (fNegative || bnTarget == 0 || fOverflow || bnTarget > Params().ProofOfWorkLimit())
-        return error("CheckProofOfWork() : nBits below minimum work");
+    if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
+        return error("CheckProofOfWork(): nBits below minimum work");
 
     // Check proof of work matches claimed amount
-    if (hash > bnTarget)
-        return error("CheckProofOfWork() : hash doesn't match nBits");
+    if (UintToArith256(hash) > bnTarget)
+        return error("CheckProofOfWork(): hash doesn't match nBits");
 
     return true;
 }
 
-uint256 GetBlockProof(const CBlockIndex& block)
+arith_uint256 GetBlockProof(const CBlockIndex& block)
 {
-    uint256 bnTarget;
+    arith_uint256 bnTarget;
     bool fNegative;
     bool fOverflow;
     bnTarget.SetCompact(block.nBits, &fNegative, &fOverflow);
     if (fNegative || fOverflow || bnTarget == 0)
         return 0;
     // We need to compute 2**256 / (bnTarget+1), but we can't represent 2**256
-    // as it's too large for a uint256. However, as 2**256 is at least as large
+    // as it's too large for a arith_uint256. However, as 2**256 is at least as large
     // as bnTarget+1, it is equal to ((2**256 - bnTarget - 1) / (bnTarget+1)) + 1,
     // or ~bnTarget / (nTarget+1) + 1.
     return (~bnTarget / (bnTarget + 1)) + 1;
+}
+
+int64_t GetBlockProofEquivalentTime(const CBlockIndex& to, const CBlockIndex& from, const CBlockIndex& tip, const Consensus::Params& params)
+{
+    arith_uint256 r;
+    int sign = 1;
+    if (to.nChainWork > from.nChainWork) {
+        r = to.nChainWork - from.nChainWork;
+    } else {
+        r = from.nChainWork - to.nChainWork;
+        sign = -1;
+    }
+    r = r * arith_uint256(params.nPowTargetSpacing) / GetBlockProof(tip);
+    if (r.bits() > 63) {
+        return sign * std::numeric_limits<int64_t>::max();
+    }
+    return sign * r.GetLow64();
 }
